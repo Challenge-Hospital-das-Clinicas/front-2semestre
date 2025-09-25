8038d12c9a71f1dca56960b3f948a483469866ab
guilhermemacedomartins <guilhernemacedomartins@gmail.com>
2025-09-25 15:34:28 -0300

    Importando os elementos de router e as paginas no main.tsx

--- challenge-front/src/main.tsx
+++ challenge-front/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './globals.css'
 
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './rotas/Home/index.tsx';
+import Error from './rotas/Error/index.tsx';
+import Contato from './rotas/Contato/index.tsx';
+import Faq from './rotas/Faq/index.tsx';
+import Tutorial from './rotas/Tutorial/index.tsx';
+import Integrantes from './rotas/Integrantes/index.tsx'
+ 
+const router = createBrowserRouter([
+  {path:"/", element: <App/>, errorElement:<Error/>, children:[
+    {path:"/", element: <Home/>},
+    {path:"/Contato", element: <Contato/>},
+    {path:"/Faq", element: <Faq/>},
+    {path:"/Equipe", element: <Integrantes/>},
+    {path:"/Tutorial", element: <Tutorial/>}
+  ]}
+]);
+ 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
+ 
