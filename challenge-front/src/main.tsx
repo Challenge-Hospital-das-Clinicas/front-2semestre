fada9c54f380cf7c2fbcba30802bcbb5139c9d1b
pedrohenrique116 <pedrohenriqueluiz9@gmail.com>
2025-11-05 18:25:34 -0300

    criando as pastas do crud do API de JAVA

--- challenge-front/src/main.tsx
+++ challenge-front/src/main.tsx
@@ -2,24 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-
+// 1. Importa o CSS Global (que importa o Tailwind)
 import './index.css'
+
+// 2. Importa o Layout Principal
 import App from './App.tsx'
 
+// 3. Importa TODAS as nossas páginas (Usando a sua estrutura 'src/rotas/')
+import Home from './rotas/home/index.tsx'
+import Contato from './rotas/contato/index.tsx'
+import Faq from './rotas/faq/index.tsx'
+import Integrantes from './rotas/integrantes/index.tsx'
+import Tutorial from './rotas/tutorial/index.tsx'
+import MarcarConsulta from './rotas/marcarConsulta/index.tsx'
+import Medico from './rotas/medicos/index.tsx' // <-- Corrigido!
 
-import Home from './rotas/Home/index.tsx'
-import Contato from './rotas/Contato/index.tsx'
-import Faq from './rotas/Faq/index.tsx'
-import Integrantes from './rotas/Integrantes/index.tsx'
-import Tutorial from './rotas/Tutorial/index.tsx'
-import MarcarConsulta from './rotas/MarcarConsulta/index.tsx'
-
-
+// 4. Cria o roteador
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-
+    element: <App />, 
+    
     children: [
       {
         path: "/",
@@ -45,11 +48,15 @@ const router = createBrowserRouter([
         path: "/marcar-consulta",
         element: <MarcarConsulta />,
       },
+      {
+        path: "/medico", // A URL que você vai acessar
+        element: <Medico />, // O componente que vai carregar (o CRUD)
+      },
     ],
   },
 ]);
 
-
+// 5. Renderiza a aplicação
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- challenge-front/src/rotas/Home/index.tsx
+++ challenge-front/src/rotas/Home/index.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 const equipePreview = [
   { name: 'Pedro Henrique' },
-  { name: 'Gabriel Hayashi' },
   { name: 'Guilherme Macedo' },
 ];
 
