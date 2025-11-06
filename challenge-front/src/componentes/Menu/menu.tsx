ad013ce5db0d9ff2f51ae369f080eb1bc9fd0d64
guilhermemacedomartins <guilhernemacedomartins@gmail.com>
2025-11-06 16:49:40 -0300

    feat: arruamndo a pagina de integrantes adicionando rm e turma e removendo o gabriel, e na pagina menu centralizando o pedro e o guilherme

--- challenge-front/src/componentes/Menu/menu.tsx
+++ challenge-front/src/componentes/Menu/menu.tsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
 
-
 type MenuProps = {
-  isMobile?: boolean; 
+  isMobile?: boolean;
 }
 
 export default function Menu({ isMobile = false }: MenuProps) {
   
-  
   const navClasses = isMobile
     ? "flex flex-col items-center p-4" 
-    : "flex items-center gap-5";     
+    : "flex items-center justify-center flex-wrap gap-x-5 gap-y-2"; 
 
   const linkClasses = isMobile
-    ? "w-full text-center py-3 text-lg hover:bg-blue-800 transition-colors" 
-    : "text-lg font-bold transition-colors hover:text-sky-300"; 
+    ? "w-full text-center py-3 text-lg hover:bg-blue-800 transition-colors"
+    : "text-lg font-bold transition-colors hover:text-sky-300";
 
   return (
     <nav className={navClasses}>
@@ -23,8 +21,6 @@ export default function Menu({ isMobile = false }: MenuProps) {
       <Link className={linkClasses} to="/faq">FAQ</Link>
       <Link className={linkClasses} to="/integrantes">Equipe</Link>
       <Link className={linkClasses} to="/tutorial">Tutorial</Link>
-      
-      {}
       <Link className={linkClasses} to="/admin">Admin</Link>
     </nav>
   );
