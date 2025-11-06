29897d614a4d275e8f0543ccf6c5c27d4c0343a3
pedrohenrique116 <pedrohenriqueluiz9@gmail.com>
2025-11-06 15:47:52 -0300

    feat: alterando o as configurações do vite config

--- challenge-front/vite.config.ts
+++ challenge-front/vite.config.ts
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+
+ 
+  server: {
+    proxy: {
+  
+      '/api': {
+      
+        target: 'https://hospitaltech-api-latest.onrender.com',
+       
+        changeOrigin: true,
+       
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
